3bbf42736aa41c55e5f4b8f7a89240fac8b55d09
Roman Andriianov <randriyanov9@gmail.com>
2019-05-05 21:36:38 +0300

    removed binary files

--- 7-create-jdk/custom-jre/JokeServer/makeJRE.sh
+++ 7-create-jdk/custom-jre/JokeServer/makeJRE.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-9.0.1.jdk/Contents/Home/
 rm -rf out/jre
-/Library/Java/JavaVirtualMachines/jdk-9.0.1.jdk/Contents/Home/bin/jlink \
-	--module-path $JAVA_HOME/jmods:out/production \
+$JAVA_HOME/bin/jlink \
+	--module-path $JAVA_HOME/jmods:out \
 	--add-modules com.ra.jokeapp.ui \
 	--launcher JOKER=com.ra.jokeapp.ui/com.ra.jokeapp.ui.Main \
 	--output out/jre
--- 7-create-jdk/custom-jre/JokeServer/src/com.ra.jokeapp.ui/com/ra/jokeapp/ui/Main.java
+++ 7-create-jdk/custom-jre/JokeServer/src/com.ra.jokeapp.ui/com/ra/jokeapp/ui/Main.java
@@ -6,10 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        final File f = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        System.out.println("Classpath is " + f.getAbsolutePath());
         Parent root = FXMLLoader.load(getClass().getResource("JokeGUI.fxml"));
         primaryStage.setTitle("Tell me a Joke");
         primaryStage.setScene(new Scene(root, 400, 250));
